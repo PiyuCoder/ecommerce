63f6b1ac6d48ad31ab191c8791a2b806ad863fff
PiyuCodere <piyushrai284@gmail.comm>
2024-01-07 22:51:33 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", express.static("build"));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(join(__dirname, "./build", "index.html"));
 });
 
