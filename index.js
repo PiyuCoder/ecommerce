9399ce10d562c330bc98eaee896493e6bfc80b41
PiyuCodere <piyushrai284@gmail.comm>
2024-01-07 16:50:18 +0530

    third

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", express.static("client/build"));
+app.use("/", express.static("build"));
 
 //Database connection
 connectDb();
